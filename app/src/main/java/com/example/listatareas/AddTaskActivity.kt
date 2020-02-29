fbd8faaf0041467721c4cf5616999aa1c8642ad5
monygl <mony_gl@hotmail.com>
2020-02-28 20:01:20 -0600

    Initial commit

---
+++ app/src/main/java/com/example/listatareas/AddTaskActivity.kt
@@ -0,0 +1,12 @@
+package com.example.listatareas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+}
